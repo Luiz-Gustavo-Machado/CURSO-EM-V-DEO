83d842b1c95cb69e14aab9b33213179f5ae52750
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-05 20:53:13 -0300

    Update index.js

--- JavaScript/AULA12ex/ex015/index.js
+++ JavaScript/AULA12ex/ex015/index.js
@@ -6,6 +6,35 @@ function verificar() {
    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
     alert('[ERRO] Verifique os dados tente novamente!')
 } else {
-    alert
-}
+    let fsex = document.getElementsByName('radsex')
+    let idade = ano - Number(fano.value)
+    let gênero = ''
+    let img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        gênero = 'Homem'
+        if (idade >=0 && idade < 10) {
+            // Criança
+        } else if (idade < 21) {
+            //Jovem
+        } else if (idade < 50) {
+             //adulto
+        } else {
+            //idoso
+        }
+    } else if (fsex[1].checked) {
+        gênero = 'Mulher'
+        if (idade >=0 && idade < 10) {
+            // Criança
+        } else if (idade < 21) {
+            //Jovem
+        } else if (idade < 50) {
+             //adulto
+        } else {
+            //idoso
+        }
+    }
+    res.getElementsByClassName.textAlin = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+}   
 }
