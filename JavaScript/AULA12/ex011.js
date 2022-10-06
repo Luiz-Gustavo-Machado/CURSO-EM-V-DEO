18176854ec03789a4f3ee6eacffc102c67993c3c
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-05 22:04:24 -0300

    Update ex011.js

--- JavaScript/AULA12/ex011.js
+++ JavaScript/AULA12/ex011.js
@@ -1,19 +1,10 @@
-/*let idade = 11
+let idade = 11
 if (idade < 16) {
     console.log('Não vota')
 } else {
     if (idade >= 16 && idade < 18) // essa linha não seria necessarios pois já esta explicito se a idade não é maior que 16 obviamente ele é maior ou igual
     console.log('Voto opcional')
 } 
-
-let idade = 11
-if (idade < 16) {
-    console.log('Não vota')
-} else if (idade < 18) {
-        console.log('Voto opcional')
-} else if (idade >= 18) { //se a idade não é menor do que 18 obviamente ela é maior e igual a 18
-        console.log('voto obrigatório')
-}
 
 
 let idade = 22
@@ -23,9 +14,9 @@ if (idade < 16) {
     console.log('Voto Opcional')
 } else {
     console.log('Voto Obrigatório')
-}*/
+}
 
-/*no Brasil, acima de 67 anos o voto volta a ser opcional
+//No Brasil, acima de 67 anos o voto volta a ser opcional
 
 let idade = 16
     console.log(`Você tem ${idade} anos.`)
@@ -35,4 +26,4 @@ if (idade < 16) {
     console.log('O Voto Opcional!')
 } else {
     console.log('O Voto Obrigatório!')
-}*/
+}
