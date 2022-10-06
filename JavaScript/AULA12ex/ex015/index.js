8a2de216018c6708ad292ca194ae3af15a77f650
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-05 21:53:46 -0300

    move

--- JavaScript/AULA12ex/ex015/index.js
+++ JavaScript/AULA12ex/ex015/index.js
@@ -1,40 +1,49 @@
 function verificar() {
-   let data = new Date()
-   let ano = data.getFullYear()
-   let fAno = document.getElementById('txtano')
-   let res = document.querySelector('div#res')
-   if (fAno.value.length == 0 || Number(fAno.value) > ano) {
-    alert('[ERRO] Verifique os dados tente novamente!')
-} else {
-    let fsex = document.getElementsByName('radsex')
-    let idade = ano - Number(fano.value)
-    let gênero = ''
-    let img = document.createElement('img')
-    img.setAttribute('id', 'foto')
-    if (fsex[0].checked) {
-        gênero = 'Homem'
-        if (idade >=0 && idade < 10) {
-            // Criança
-        } else if (idade < 21) {
-            //Jovem
-        } else if (idade < 50) {
-             //adulto
-        } else {
-            //idoso
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+         alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                 // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                 //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }   
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+             if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
         }
-    } else if (fsex[1].checked) {
-        gênero = 'Mulher'
-        if (idade >=0 && idade < 10) {
-            // Criança
-        } else if (idade < 21) {
-            //Jovem
-        } else if (idade < 50) {
-             //adulto
-        } else {
-            //idoso
-        }
-    }
-    res.getElementsByClassName.textAlin = 'center'
-    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-}   
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }  
 }
