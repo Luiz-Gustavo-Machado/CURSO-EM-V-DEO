2045a480ac7e7dac669c9a372525e8153d96abd1
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-06 15:20:35 -0300

    Update teste.js

--- JavaScript/AULA14/teste.js
+++ JavaScript/AULA14/teste.js
@@ -1,5 +1,13 @@
 // Ultilizando o FOR
 
-for(var c = 1; c <=6; c++) {
+/*for(var c = 1; c <=6; c++) {
+    console.log(c)
+}*/
+
+// 
+
+console.log('Vai começar...')
+for(var c = 1; c <=4; c++) {
     console.log(c)
 }
+console.log('FIM!')
