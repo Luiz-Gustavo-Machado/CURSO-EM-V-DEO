a603a72ed6429bb3965c5959d3ff7ba5c4410118
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-06 12:20:43 -0300

    upload new folder

---
+++ JavaScript/AULA13/ambiente.js
@@ -0,0 +1,15 @@
+// usando a cabeça na programação usando o comendo de repetição
+
+let c = 1
+while (c <= 6) {
+    console.log('Tudo bem!')
+}
+
+
+// usando a força bruta para programar as repetiçoes hehe...
+/*console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
