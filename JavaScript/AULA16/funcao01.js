85873aeaf484f8043cfcc76e1fbeee336bcc11f3
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-07 17:52:45 -0300

    upload

---
+++ JavaScript/AULA16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+/*let res = parimpar(223)
+console.log(res)*/ //podemos fazer nesse modelo expressando a variavel ou somente colocar dessa forma ABAIXO!
+           //OU
+console.log(parimpar(223))
+
---
+++ JavaScript/AULA16/funcao02.js
@@ -0,0 +1,12 @@
+/*function soma(n1, n2) {
+    return n1 + n2
+}                            //o resultado daria 7
+
+console.log(soma(2, 5))*/
+
+function soma(n1, n2) {
+    return n1 + n2
+} 
+                            // caso colocarmos apenas definirmos um nomero 
+console.log(soma(2))        // um dos valores vai dar undefynit e o 
+                            //  resultado   será NaN     
