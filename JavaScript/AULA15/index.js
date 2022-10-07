e73b1e694de10204e9fb8bc0b1d30ed76dc40f36
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-07 15:54:05 -0300

    Update index.js

---
+++ JavaScript/AULA15/index.js
@@ -0,0 +1,24 @@
+/*let num = [5, 7 , 2, 9, 3]
+
+num.push(1) //acrescentou o numero na ultima posição
+num.sort() // colocou os numero em ordem 
+console.log(num) 
+console.log(`O vetor tem ${num.length} posiçÔes`)
+console.log(`Ò primeiro valor do vetor é ${num[0]}`)*/
+
+
+// outras que é muito comum de se fazer com o vetor é mostrar ele na tela sem a formatação padrão pq geralmente ele mostra aquela formatação padrão bem bagunçada com colchete, se você quiser mostrar do seu jeitinho da maneira que você preferir tem o jeito
+
+/*let valores = [5, 7 , 2, 9, 3]
+
+valores.sort()*/
+/*for(let pos=0; pos < valores.length; pos++) {
+    conso4dxxle.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+// podemos simplificar a variavel no for 
+
+/*for(let pos in valores) {
+    console.log(valores[pos])
+}*/
+
