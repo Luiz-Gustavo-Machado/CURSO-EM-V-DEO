a2d49e982dbbb25f818e1bd044805b758814e85b
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-07 18:00:06 -0300

    upload

--- JavaScript/AULA16/funcao02.js
+++ JavaScript/AULA16/funcao02.js
@@ -4,9 +4,16 @@
 
 console.log(soma(2, 5))*/
 
-function soma(n1, n2) {
+/*function soma(n1, n2) {
     return n1 + n2
 } 
-                            // caso colocarmos apenas definirmos um nomero 
-console.log(soma(2))        // um dos valores vai dar undefynit e o 
-                            //  resultado   será NaN     
+                              // caso colocarmos apenas definirmos um nomero 
+console.log(soma(2))*/        // um dos valores vai dar undefynit e o 
+                              //  resultado   será NaN Not number
+
+ 
+function soma(n1=0, n2=0) { 
+    return n1 + n2
+}                              //podemos resolver essa situação da falta 
+                               // de um outro valor dessa seguinte forma
+console.log(soma(2))           // colocando o recebe = 0 
