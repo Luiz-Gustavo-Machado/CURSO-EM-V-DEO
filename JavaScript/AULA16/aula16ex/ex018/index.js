c1576c124485ad34f3d6fc25f716addd83de2f08
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-10-10 17:11:35 -0300

    upload

--- JavaScript/AULA16/aula16ex/ex018/index.js
+++ JavaScript/AULA16/aula16ex/ex018/index.js
@@ -11,8 +11,8 @@ function isNumero(n) {
     }
 }
 
-function inLista(n, 1) {
-    if (lista.indexOf(Number(n)) != -1) {
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
@@ -20,9 +20,36 @@ function inLista(n, 1) {
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value)) {
-        return true
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        return false
+        alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antres de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        res.innerHTML =''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor infomado foi ${maior}.</p>`
+        res.innerHTML += `<p> O manor valor in omado foi ${menor}.</p>`
     }
 }
